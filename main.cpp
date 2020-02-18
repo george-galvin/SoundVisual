1f7816d5c0c4b2a7157125a767eaf426fee06ec0
George Galvin <george.galvin1996@gmail.com>
2020-02-18 22:11:13 +0000

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
-#include "SDL.h"
-#include <windows.h>
-#include <iostream>
+/* Simple LED Music Visualiser - PC side.
+ * Takes input from Stereo Mix (using SDL), a feature on most old Windows computers that lets you work with
+ * the output audio going to the speakers in real-time. Turns it into data to be sent over the 
+ * serial port to an Arduino, to control the colours of an LED array. 
+ */
 
-const int SAMPLES_PER_FRAME = 2048;
-double speed = 5.0;
+#include "SDL.h" //to get input audio
+#include <windows.h> //to output data to serial port
 
-int data_count = 0;
-const int RECORDING_BUFFER_SECONDS = 100000;
+const int SAMPLES_PER_FRAME = 2048; 
+const int SAMPLE_FREQ = 44100; //i.e. about 22 transmissions per second
 
+int data_count = 0; //total number of bytes sent
+const int RECORDING_BUFFER_SECONDS = 100000; //Necessary argument to the SDL audio function
+
+//Necessary variables for the SDL function3
 Sint16* gRecordingBuffer = NULL;
 Uint32 gBufferBytePosition = 0;
 
+//Necessary variables for the serial port function
 DCB serialParams;
 DWORD bytessent;
 byte data[1];
@@ -18,24 +25,34 @@ HANDLE arduino;
 
 void one_byte_callback( void* userdata, Uint8* stream, int bytes )
 {
+//The callback function, executed each time a frame's worth of new samples 
+// comes in from Stereo Mix.
+
     data_count++;
-    Sint16* real_stream = (Sint16*)stream;
+    Sint16* real_stream = (Sint16*)stream; //turn data back to 16-bit form
     int length = bytes/2;
     int mod_sum = 0;
 
-    for (int i = 0; i < length; i++)
+    for (int i = 0; i < length; i++) //get the average amplitude of the sound wave
     {
         mod_sum += abs(real_stream[i]);
     }
-    data[0] = ((mod_sum / (8*length)) + data_count) % 255;
-    WriteFile(arduino, &data, 1, &bytessent, 0);
+
+    data[0] = ((mod_sum / (8*length)) + data_count) % 255; 
+    //Turn this into a byte, representing a saturated colour on the HSV wheel.
+    //The data_count variable is added to continually change the colour scale of the array
+    //to make it interesting. The (8*length) part can be scaled to make the colour changes more
+    //or less subtle.
+
+    WriteFile(arduino, &data, 1, &bytessent, 0); //Send this into 
 }
 
 void init()
 {
-    SDL_Init(SDL_INIT_AUDIO);
+    //Initialise SDL
+    SDL_Init(SDL_INIT_AUDIO); 
 
-    //Initialise arduino
+    //Initialise arduino (the virtual port name "/COM5" is different on different machines)
     arduino = CreateFile("/COM5", GENERIC_WRITE, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
     serialParams.BaudRate = CBR_19200;
     serialParams.ByteSize = 8;
@@ -44,8 +61,7 @@ void init()
 
     SetCommState(arduino, &serialParams);
 
-    //Initialise audio
-
+    //Initialise connection to Stereo Mix
     SDL_AudioSpec desiredRecordingSpec;
     SDL_zero(desiredRecordingSpec);
     SDL_AudioSpec gReceivedRecordingSpec;
@@ -86,7 +102,7 @@ int main( int argc, char* args[] )
     {
         while( SDL_PollEvent( &e ) != 0 )
         {
-            if( e.type == SDL_KEYDOWN )
+            if( e.type == SDL_KEYDOWN ) //Wait for key down on console
             {
                 quit = true;
             }
